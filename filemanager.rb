69923f99694c5a25c11d0ab0a5704fb79db8e47c
Apprentice <apprentice@dbc25.local>
2013-06-14 12:10:09 -0700

    Finished filemanager format

--- filemanager.rb
+++ filemanager.rb
@@ -1,26 +1,41 @@
-
+require_relative 'flashcards.rb'
 
 class Filemanager
-
+  attr_reader :flashcards
   def initialize
-    @questions = []
-    @answers   = []
-    @answers << "what up"
+    @questions  = []
+    @answers    = []
+    @pre_format = []
+    @flashcards = []
     get_files
+    pre_format
+    set_objects
   end
 
   def get_files
-    IO.foreach("flashcard_sample.txt") do |line|
+    IO.foreach("flashcard_samples.txt") do |line|
       # p line.chomp
     # p line.is_a? 
     # puts @answers
-    @answers << line.chomp unless line.chomp == ""
+    @pre_format << line.chomp unless line.chomp == ""
   end
-  # puts @answers.count
-  # puts @answers.compact!
-  puts @answers.count
-  
+end
+
+def pre_format
+  @pre_format.each_with_index do |value, index|
+    @questions << value if index.even?
+    @answers   << value if index.odd?
   end
+end
+
+def set_objects
+  @questions.each_with_index do |this_question, index|
+    @flashcards << Flashcard.new(this_question, @answers[index])
+  end
+
+end
+
+
 
 end
 
