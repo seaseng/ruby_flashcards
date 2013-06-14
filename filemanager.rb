95b9709ac34e30e9bb75ba6880ed62d9f8fbde27
Apprentice <apprentice@dbc25.local>
2013-06-14 14:54:10 -0700

    Refactored filemanager for YAML

--- filemanager.rb
+++ filemanager.rb
@@ -2,27 +2,24 @@ require_relative 'flashcards.rb'
 
 class Filemanager
   attr_reader :flashcards
-  def initialize
-    @questions  = []
-    @answers    = []
-    @pre_format = []
-    @flashcards = []
+  def initialize 
+    @pre_format_list  = []
+    @flashcards       = []
     get_files
-    pre_format
+    seperate_input_array
     set_objects
   end
 
   def get_files
     IO.foreach("flashcard_samples.txt") do |line|
-      # p line.chomp
-    # p line.is_a? 
-    # puts @answers
-    @pre_format << line.chomp unless line.chomp == ""
+    @pre_format_list << line.chomp unless line.chomp == ""
   end
 end
 
-def pre_format
-  @pre_format.each_with_index do |value, index|
+def seperate_input_array
+  @questions  = []
+  @answers    = []
+  @pre_format_list.each_with_index do |value, index|
     @questions << value if index.even?
     @answers   << value if index.odd?
   end
@@ -33,8 +30,18 @@ def set_objects
     @flashcards << Flashcard.new(this_question, @answers[index])
   end
 
+  def save_to_file
+    File.open('./bingo.yml','w') do |config|
+      config..to_yaml
+    end
+  end
+
+  def load_file
+    @bingo = YAML::load(File.open('./bingo.yml','r'))
+  end
+
+  end
+
 end
 
-
-
-end
+# x = Filemanager.new
