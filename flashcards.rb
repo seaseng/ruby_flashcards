c274eb5d0d7053bd6582b8997e6db1350ce71c48
Apprentice <apprentice@devbootcamp.com>
2013-06-14 11:25:16 -0700

    created flashcard class

--- flashcards.rb
+++ flashcards.rb
@@ -1,1 +1,18 @@
 # Main file for 
+
+class Flashcard
+  attr_reader :question, :answer
+
+  def initialize(question, answer)
+
+    @question = question
+    @answer = answer
+  end
+
+  def to_s
+    "Question: #{self.question}\nAnswer: #{self.answer}"
+  end
+end
+
+
+# test = Flashcard.new("Q", "A")
